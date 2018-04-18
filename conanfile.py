567ad925ff3557ee03a0a27040dfcc0f2b3a6b43
Henry Kielmann <henrykielmann@gmail.com>
2018-04-18 16:22:12 +0200

    .

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from shutil import move
 
 class LiveShareClient(ConanFile):
     name = 'live-share-client'
-    requires = 'Qt/[~=5.10]@sogilis/testing'
+    requires = 'Qt/[~=5]@henry4k/testing'
     generators = 'pkg_config'
 
     def imports(self):
